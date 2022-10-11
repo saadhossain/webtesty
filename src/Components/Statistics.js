4d8f2972694eef53d74453d3d998e2d9dc18a254
saadhossain <mshsarkar@gmail.com>
2022-10-11 11:53:32 +0600

    Statistics added with bar chart

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { QuizContext } from '../Layout/Root';
 
 const Statistics = () => {
@@ -6,8 +7,22 @@ const Statistics = () => {
     const quizData = quiz.data;
     console.log(quizData);
     return (
-        <div>
-            <h1>Statistics</h1>
+        <div className='w-10/12 md:w-2/4 mx-auto text-center'>
+            <h1 className='text-2xl font-bold my-10'>Quiz Statistics</h1>
+            <div className='my-10'>
+                <ResponsiveContainer width="100%" height={300}>
+                    <BarChart data={quizData}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="name" fill="#8884d8" />
+                        <Bar dataKey="total" fill="#82ca9d" />
+                    </BarChart>
+                </ResponsiveContainer>
+
+            </div>
         </div>
     );
 };
